619b0df80cebd8d3c6d3310b2ec11ce106deaf97
henriqueandrade <henriqueandrade_@hotmail.com>
2023-09-13 20:58:14 +0200

    Fix 202309 1

--- scripts/pages.js
+++ scripts/pages.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
     var topBarHeight = $('#top-bar').height();
     $('#icon-top-bar').width($('#top-bar').height());
     $('.nav-button').height(topBarHeight);
-    // $('.icon').each(function () {
-    //     $(this).width($(this).parent().height());
-    // });
 
     $('.button, .clickable, .nav-button').click(function (event) {
         // Navigation
